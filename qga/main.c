80f21122f4adff234c47ed201b2dcccd455ea75d
ztt199 <48272263+ztt199@users.noreply.github.com>
2021-03-16 15:52:17 +0800

    Update main.c

--- qga/main.c
+++ qga/main.c
@@ -265,7 +265,7 @@ static void ga_log(const gchar *domain, GLogLevelFlags level,
                    const gchar *msg, gpointer opaque)
 {
     GAState *s = opaque;
-    GTimeVal time;
+    GDateTime time;
     const char *level_str = ga_log_level_str(level);
 
     if (!ga_logging_enabled(s)) {
@@ -280,7 +280,7 @@ static void ga_log(const gchar *domain, GLogLevelFlags level,
 #else
     if (level & s->log_level) {
 #endif
-        g_get_current_time(&time);
+        g_get_real_time(&time);
         fprintf(s->log_file,
                 "%lu.%lu: %s: %s\n", time.tv_sec, time.tv_usec, level_str, msg);
         fflush(s->log_file);
